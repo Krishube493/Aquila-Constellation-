db38aa235fbaff4059b2f45d9510069a5d8448c7
Krishube493 <krishube493@ed.amdsb.ca>
2018-02-21 13:35:24 -0500

    Almost Complete

--- Aquila(Constellation)/Form1.Designer.cs
+++ Aquila(Constellation)/Form1.Designer.cs
@@ -28,23 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.starLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // starLabel
+            // 
+            this.starLabel.AutoSize = true;
+            this.starLabel.Font = new System.Drawing.Font("Mistral", 108.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.starLabel.Location = new System.Drawing.Point(246, 191);
+            this.starLabel.Name = "starLabel";
+            this.starLabel.Size = new System.Drawing.Size(173, 173);
+            this.starLabel.TabIndex = 0;
+            this.starLabel.Text = "★";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Freestyle Script", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(221, 113);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(243, 47);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Aquila, The Eagle ";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Freestyle Script", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(257, 378);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(148, 32);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Kristianna Huber";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(634, 461);
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.starLabel);
             this.ForeColor = System.Drawing.SystemColors.Control;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label starLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label nameLabel;
     }
 }
 
